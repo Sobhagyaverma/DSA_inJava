e0c2fccdd538a1ae0b611d12be892c88b6328ae3
sobhagyaverma <sobhagyaverma16@gmail.com>
2025-08-24 20:13:06 +0530

    75. Sort color

---
+++ Arrays/SortColor.java
@@ -0,0 +1,49 @@
+package Arrays;
+
+import java.util.*;
+
+public class SortColor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sortcolor(arr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(" " + arr[i]);
+        }
+
+    }
+
+    public static void sortcolor(int[] arr) {
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                int temp = arr[mid];
+                arr[mid] = arr[low];
+                arr[low] = temp;
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+
+            }
+        }
+
+    }
+
+}
