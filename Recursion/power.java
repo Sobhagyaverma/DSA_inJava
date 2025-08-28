2f746d75bd8a082b85a95b60d8db4f1485ad11c9
sobhagyaverma <sobhagyaverma16@gmail.com>
2025-08-28 14:14:28 +0530

    basic recursion

---
+++ Recursion/power.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class power {
+    public static void main(String[] args) {
+        int n = 5;
+        int ans = power(n);
+        System.out.println(ans);
+    }
+
+    public static int power(int n) {
+
+    }
+
+}
---
+++ Recursion/sumOfArray.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class sumOfArray {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 6 };
+        int n = arr.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println(sum);
+    }
+}
